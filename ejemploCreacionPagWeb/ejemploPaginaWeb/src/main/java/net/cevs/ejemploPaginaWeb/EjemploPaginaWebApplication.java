b35cb08be9ed5528441bc73253918fe38e01b6d1
SCOOP <SCOOP@Carlos>
2020-07-14 15:59:37 -0500

    Proyecto primera version

---
+++ ejemploCreacionPagWeb/ejemploPaginaWeb/src/main/java/net/cevs/ejemploPaginaWeb/EjemploPaginaWebApplication.java
@@ -0,0 +1,13 @@
+package net.cevs.ejemploPaginaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploPaginaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploPaginaWebApplication.class, args);
+	}
+
+}
---
+++ ejemploCreacionPagWeb/ejemploPaginaWeb/src/test/java/net/cevs/ejemploPaginaWeb/EjemploPaginaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cevs.ejemploPaginaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploPaginaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
